68ec6b9bc3899b2c622b54f1c9db3714a7ab0cf4
Harold Hannon <haroldhannon@hotmail.com>
2018-11-26 16:21:14 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get install -y python
+apt-get install -y python-pip
+apt-get install -y wget 
+mkdir conductor
+cd conductor
+wget https://raw.githubusercontent.com/ibivibiv/scripts/master/conductor/__init__.py
+wget https://raw.githubusercontent.com/ibivibiv/scripts/master/conductor/conductor.py
+cd ..
+wget https://raw.githubusercontent.com/ibivibiv/scripts/master/brutal_worker.py
+wget https://raw.githubusercontent.com/ibivibiv/scripts/master/brutal_worker.py
