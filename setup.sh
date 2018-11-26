aff1088849e428eb9785c4022626ca578b47666b
Harold Hannon <haroldhannon@hotmail.com>
2018-11-26 16:25:02 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 apt-get install -y python
 apt-get install -y python-pip
 apt-get install -y wget
+pip install requests
 mkdir conductor
 cd conductor
 wget https://raw.githubusercontent.com/ibivibiv/scripts/master/conductor/__init__.py
