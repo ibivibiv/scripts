fb5234f1a4074f642eb7d77bc7b102f5add8f633
Harold Hannon <haroldhannon@hotmail.com>
2018-11-26 16:48:34 -0600

    Update brutal_worker.py

--- brutal_worker.py
+++ brutal_worker.py
@@ -5,46 +5,47 @@ from conductor import conductor
 
 file = open(str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))+".csv", "w")
 
-try:
-    base = 'http://169.46.11.166:8080/api'
+while True:
+    try:
+        base = 'http://169.46.11.166:8080/api'
 
-    pp = pprint.PrettyPrinter()
+        pp = pprint.PrettyPrinter()
 
-    workflowClient = conductor.WorkflowClient(base)
+        workflowClient = conductor.WorkflowClient(base)
 
-    taskClient = conductor.TaskClient(base)
+        taskClient = conductor.TaskClient(base)
 
-    metaClient = conductor.MetadataClient(base)
+        metaClient = conductor.MetadataClient(base)
 
-    while True:
+    
 
         workflow_list = workflowClient.getRunningWorkflows(wfName="Abuser Test Workflow")
 
         for wfId in workflow_list:
 
-            workflow = workflowClient.getWorkflow(wfId=wfId, includeTasks=True)
+                workflow = workflowClient.getWorkflow(wfId=wfId, includeTasks=True)
 
-            for task in workflow['tasks']:
-                workflowId = str(task['workflowInstanceId']).strip()
-                taskId = str(task['taskId']).strip()
+                for task in workflow['tasks']:
+                    workflowId = str(task['workflowInstanceId']).strip()
+                    taskId = str(task['taskId']).strip()
 
-                taskObj = taskClient.getTask(taskId)
-                taskObj['status'] = "COMPLETED"
-                taskObj['workerId'] = "test"
-                try:
-                    response = taskClient.ackTask(taskId, "brutalizer")
-                    response = taskClient.updateTask(taskObj=taskObj)
-                    pp.pprint("completed")
-                    file.write(str(time.time())+", taskcomplete \r")
-                except Exception as e:
-                    if "Extra data" in str(e) or "Expecting" in str(e):
+                    taskObj = taskClient.getTask(taskId)
+                    taskObj['status'] = "COMPLETED"
+                    taskObj['workerId'] = "test"
+                    try:
+                        #response = taskClient.ackTask(taskId, "brutalizer")
+                        response = taskClient.updateTask(taskObj=taskObj)
                         pp.pprint("completed")
-                        file.write(str(time.time()) + ", taskcomplete \r")
-                    else:
-                        pp.pprint(e)
-                        file.write(str(time.time()) + ", taskerror \r")
-except Exception as e:
-    pass
+                        file.write(str(time.time())+", taskcomplete \r")
+                    except Exception as e:
+                        if "Extra data" in str(e) or "Expecting" in str(e) or "JSON" in str(e):
+                            pp.pprint("completed")
+                            file.write(str(time.time()) + ", taskcomplete \r")
+                        else:
+                            pp.pprint(e)
+                            file.write(str(time.time()) + ", taskerror \r")
+    except Exception as e:
+        pass
 
-finally :
-    file.close()
+    finally :
+        file.close()
